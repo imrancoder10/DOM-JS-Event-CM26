6ad1d41c2691e0565fe3f4df3463971991472379
imrancoder10 <imranhossain1148@gmail.com>
2024-09-16 12:09:49 +0600

    4 types of Events handeler

--- js/events.js
+++ js/events.js
@@ -1,6 +1,9 @@
 // option 1: directly set on the HTML Element.
+//  <button onclick="console.log(65)">Another</button>
 
-// option 2:
+// option 2: add onclick function on the HTLM element
+// Important: we will use this
+//  <button onclick="makeRed()">Make Red</button>
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
@@ -13,8 +16,31 @@ function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 
-// option 4:
+// option 3 another:
 const makePurplebtn = document.getElementById('make-purple');
-        makePurplebtn.onclick = function makePurple(){
-            document.body.style.backgroundColor= 'purple';
-        }
+makePurplebtn.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
+}
+
+// option 4:
+const pinkBtn = document.getElementById('make-pink');
+pinkBtn.addEventListener('click', makePink)
+
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option 4 another
+
+const makeGreenBtn = document.getElementById('make-green');
+makeGreenBtn.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+
+});
+
+// option 4 Final
+// Important: we will use this sometimes
+ // document.getElementById('golden-rod').addEventListener('click', function(){});
+ document.getElementById('golden-rod').addEventListener('click', function(){
+    document.body.style.backgroundColor= 'goldenrod';
+});
