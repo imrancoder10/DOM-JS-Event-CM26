ef7a4e7cb996158bb03d4c056cbf439b68335943
imrancoder10 <imranhossain1148@gmail.com>
2024-09-16 11:45:43 +0600

    4 types onclick Events

--- js/events.js
+++ js/events.js
@@ -1,1 +1,20 @@
-console.log('js');
+// option 1: directly set on the HTML Element.
+
+// option 2:
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// option 3:
+const makeBluebtn = document.getElementById('make-blue');
+makeBluebtn.onclick = makeBlue;
+
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
+
+// option 4:
+const makePurplebtn = document.getElementById('make-purple');
+        makePurplebtn.onclick = function makePurple(){
+            document.body.style.backgroundColor= 'purple';
+        }
